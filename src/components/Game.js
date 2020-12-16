17fe31f6f8987dd7943a07a989236aa0034858af
sachinm78 <sachinm78@gmail.com>
2020-12-16 16:15:52 -0600

    created game logic using React hooks

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { calculateWinner } from '../helpers';
 import Board from './Board';
 
 const Game = () => {
-    
-    const handleClick = () => {
-        
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(board);
+
+    const handleClick = i => {
+        const boardCopy = [...board];
+        // If user click an occupied square or if game is won, return
+        if (winner || boardCopy[i]) return;
+        // Put an X or an O in the clicked square
+        boardCopy[i] = xIsNext ? 'X' : 'O';
+        setBoard(boardCopy);
+        setXisNext(!xIsNext);
     }
-    
+
     const jumpTo = () => {
-        
+
     }
-    
+
     const renderMoves = () => {
-        
+
     }
-    
+
     return (
-        <Board onClick={handleClick} />
+        <Board squares={board} onClick={handleClick} />
     )
 }
 
