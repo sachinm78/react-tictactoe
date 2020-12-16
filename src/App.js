1396d0301efbf4ac9d71ab5bad2ec6577d40c29e
sachinm78 <sachinm78@gmail.com>
2020-12-16 16:46:15 -0600

    finished app.  Set it back up to play time travel version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import Game from './components/Game';
-// import TimeTravelGame from './components/TimeTravelGame'
+// import Game from './components/Game';
+import TimeTravelGame from './components/TimeTravelGame'
 
-const App = () => <Game />;
+const App = () => 
+    <div>
+        {/* <Game /> */}
+        <TimeTravelGame />
+    </div>;
 
 export default App;
