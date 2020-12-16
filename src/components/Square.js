b799bb8e16a6eb2ab5393d5cadb72f9e8fce9d1a
sachinm78 <sachinm78@gmail.com>
2020-12-16 15:44:09 -0600

    created basic style for game board.  Used React feature that lets you style directly within the component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+const style = {
+    background: 'lightblue',
+    border: '2px solid darkblue',
+    fontSize: '45px',
+    fontWeight: '900',
+    cursor: 'pointer',
+    outline: 'none'
+};
+
 const Square = ({ value, onClick }) => (
-    <button onClick={onClick}>
+    <button style={style} onClick={onClick}>
         {value}
     </button>
 );
